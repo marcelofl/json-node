e38e6850a52a5a7a3b3c8dc3b347f43149efbfcc
marcelofl <marcelodelima.m@gmail.com>
2021-03-07 20:32:45 -0300

    config port

--- app.js
+++ app.js
@@ -11,21 +11,19 @@ app.use(cors({
 
 // *** Atividade 05 ***
 
-//Questão 1
+//Quest 1
 //Rota lista de alunos 
 app.get('/alunos/todos', (req, res) => {
 	let alunos = [
-		{'Nome': 'Marcelo', 'Curso':'Sistemas de Informação'},
-		{'Nome': 'Francisco', 'Curso': 'Sistemas de Informação'},
-		{'Nome': 'Renata', 'Curso': 'Sistemas de Informação'}	
+		{'Nome': 'Marcelo', 'Curso':'Sistemas de Informacao'},
+		{'Nome': 'Francisco', 'Curso': 'Sistemas de Informacao'},
+		{'Nome': 'Renata', 'Curso': 'Sistemas de Informacao'}	
 	]
 	res.json(alunos)
 })
 
-//Questão 2
-// Rota responsável por receber o valor a ser convertido
+//Quest 2
+// Rota valor a ser convertido
 app.get('/moeda/:valor_real', convertModel.obterValor)
 
-app.listen((process.env.PORT || 3000), function() {
-	console.log('App rodando na porta 3000!')
-})	
+app.listen(process.env.PORT || 3000)
